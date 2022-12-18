bb33cb3bffa554e06f52f688508f60d53d9a0609
FaranMazhar <60270937+FaranMazhar@users.noreply.github.com>
2022-12-18 23:15:23 +0500

    added repos to DI

--- Nastran.Web/Program.cs
+++ Nastran.Web/Program.cs
@@ -3,11 +3,17 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Nastran.DataService.DbContext;
 using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
+using Nastran.DataService.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddScoped<CategoryRepository>();
+builder.Services.AddScoped<SubCategoryRepository>();
+builder.Services.AddScoped<ProductRepository>();
+builder.Services.AddScoped<ProductImageRepository>();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
